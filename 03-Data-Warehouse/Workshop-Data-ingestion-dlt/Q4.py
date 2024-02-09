0c28ba264403bfd15b29102a38ce3adbeb97bcef
Abubakrmali2 <155449010+Abubakrmali2@users.noreply.github.com>
2024-02-09 12:27:05 +0000

    update Q4

--- 03-Data-Warehouse/Workshop-Data-ingestion-dlt/Q4.py
+++ 03-Data-Warehouse/Workshop-Data-ingestion-dlt/Q4.py
@@ -27,8 +27,17 @@ info= generators_pipeline.run (people_2(),
 conn = duckdb.connect(f"{generators_pipeline.pipeline_name}.duckdb")
 conn.sql(f"SET search_path = '{generators_pipeline.dataset_name}'")
 
+
+conn.sql(" CREATE or replace TABLE people_merge (id INTEGER PRIMARY KEY, Name VARCHAR, age INTEGER, city VARCHAR) ")
 display(conn.sql("show tables"))
 
 
-rides1 = conn.sql("Select sum(l.age) From people2 l LEFT JOIN people1 r on l.id=r.id ").df()
-display(rides1)
+conn.sql('INSERT INTO people_merge (id, name, age, city) SELECT id, name, age, city FROM people1')
+conn.sql("INSERT INTO people_merge (id, name, age, city) SELECT id, name, age, city FROM people2 ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name,age = EXCLUDED.age,city = EXCLUDED.city")
+
+
+rides= conn.sql("select * from people_merge").df()
+display(rides)
+
+rides= conn.sql("select sum(age) from people_merge").df()
+display(rides)
